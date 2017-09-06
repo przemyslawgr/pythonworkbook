34926cb608ff5b76ed3ce972e8d4d003896a4afd
Przemysław Grenda <PrzemyslawGr@advaoptical.com>
2017-09-06 10:33:46 +0200

    first ex

---
+++ ex7.py
@@ -0,0 +1,6 @@
+n = None
+n = int(input("Write a positive n and I will give you the sum of the int from 1 to n!"))
+if n > 0:
+    sum = (n*(n+1))/2
+    print("Sum =", sum)
+else: print("This is not positive num.")
